ded8aaafa16f278f9ca51cf955cc51c30349a1fc
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2026-09-10 13:42:27 +0200

    Add packages

---
+++ src/calamares-config/files/calamares/launch.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+## Copyright (C) 2020-2026 Aditya Shakya <adi1090x@gmail.com>
+
+## Check if booted using 'Copy to RAM' mode
+DIR="/etc/calamares"
+KERNEL=`uname -r`
+
+if [[ -d "/run/archiso/copytoram" ]]; then
+	sudo sed -i -e 's|/run/archiso/bootmnt/arch/x86_64/airootfs.sfs|/run/archiso/copytoram/airootfs.sfs|g' "$DIR"/modules/unpackfs.conf
+	sudo sed -i -e "s|/run/archiso/bootmnt/arch/boot/x86_64/vmlinuz-linux|/usr/lib/modules/$KERNEL/vmlinuz|g" "$DIR"/modules/unpackfs.conf
+fi
+
+## Launch calamare installer accordingly
+if [[ "$XDG_SESSION_TYPE" == 'wayland' ]]; then
+	sudo -E calamares -d -style kvantum
+else
+	pkexec calamares -d -style kvantum
+fi
